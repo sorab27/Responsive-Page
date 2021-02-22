7244335270679c046aebd77aba8be6b951ac0b7f
Sorab <sorab.khanna882@gmail.com>
2021-02-22 20:25:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains the assignment of Responsive Page.
---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    // Burger Animation
+
+    burger.classList.toggle("cross");
+  });
+};
+navSlide();
